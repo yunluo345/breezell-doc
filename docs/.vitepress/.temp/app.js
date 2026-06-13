4966467827defbd3bb213396f96d7298617c757d
yunluo345 <null>
2026-06-12 22:37:19 -0900

    new

--- docs/.vitepress/.temp/app.js
+++ docs/.vitepress/.temp/app.js
@@ -3038,7 +3038,7 @@ const _sfc_main$r = /* @__PURE__ */ defineComponent({
   __name: "VPNavBarSearch",
   __ssrInlineRender: true,
   setup(__props) {
-    const VPLocalSearchBox = defineAsyncComponent(() => import("./VPLocalSearchBox.dwUCX1DQ.js"));
+    const VPLocalSearchBox = defineAsyncComponent(() => import("./VPLocalSearchBox.B8uOEEyb.js"));
     const VPAlgoliaSearchBox = () => null;
     const { theme: theme2 } = useData();
     const loaded = ref(false);
